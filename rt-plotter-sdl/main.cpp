32906d9f728e8661b506ec15586a6fc7e11049bc
ocanath <ocanath@gmail.com>
2024-10-19 14:05:49 -0700

    keyboard accumulation unbounded

--- rt-plotter-sdl/main.cpp
+++ rt-plotter-sdl/main.cpp
@@ -48,207 +48,6 @@ uint32_t fletchers_checksum32(uint32_t* arr, int size)
 	return fchk;
 }
 
-
-/*
-* Inputs:
-*	input_buf: raw unstuffed data buffer
-* Outputs:
-*	parsed_data: floats, parsed from input buffer
-* Returns: number of parsed values
-*/
-void parse_PPP_values_noscale(uint8_t* input_buf, int payload_size, float* parsed_data, int * parsed_data_size)
-{
-	uint32_t* pbu32 = (uint32_t*)(&input_buf[0]);
-	int32_t* pbi32 = (int32_t*)(&input_buf[0]);
-	int wordsize = payload_size / sizeof(uint32_t);
-	int i = 0;
-	for (i = 0; i < wordsize; i++)
-	{
-		parsed_data[i] = ((float)pbi32[i]);
-		//printf("%d ", pbi32[i]);
-	}
-	//printf("\n");
-	//parsed_data[i] = ((float)pbu32[i]) / 1000.f;
-
-	*parsed_data_size = wordsize;
-}
-
-
-/*
-* Inputs:
-*	input_buf: raw unstuffed data buffer
-* Outputs:
-*	parsed_data: floats, parsed from input buffer
-* Returns: number of parsed values
-*/
-void parse_PPP_values(uint8_t* input_buf, int payload_size, float* parsed_data, int * parsed_data_size)
-{
-	uint32_t* pbu32 = (uint32_t*)(&input_buf[0]);
-	int32_t* pbi32 = (int32_t*)(&input_buf[0]);
-	int wordsize = payload_size / sizeof(uint32_t);
-	int i = 0;
-	for (i = 0; i < wordsize - 1; i++)
-	{
-		parsed_data[i] = ((float)pbi32[i]);
-		//printf("%d ", pbi32[i]);
-	}
-	//printf("\n");
-	parsed_data[i] = ((float)pbu32[i]) / 1000.f;
-
-	*parsed_data_size = wordsize;
-}
-
-#define PLD_SIZE	28	//bytes
-typedef union 
-{
-	uint8_t u8[PLD_SIZE];
-	int16_t i16[PLD_SIZE / sizeof(int16_t)];
-}packed_sensor_data_t;
-packed_sensor_data_t gl_tx_buf = { 0 };
-
-
-void offaxis_encoder_parser(HANDLE* pSer)
-{
-	int pld_size = 0;
-	while (1)
-	{
-		LPDWORD num_bytes_read = 0;
-		pld_size = 0;
-		int rc = ReadFile(*pSer, gl_ser_readbuf, 512, (LPDWORD)(&num_bytes_read), NULL);	//should be a DOUBLE BUFFER!
-		for (int i = 0; i < (int)num_bytes_read; i++)
-		{
-			uint8_t new_byte = gl_ser_readbuf[i];
-			pld_size = parse_PPP_stream(new_byte, gl_tx_buf.u8, sizeof(packed_sensor_data_t), gl_ppp_unstuffing_buffer, UNSTUFFING_BUFFER_SIZE, &gl_ppp_bidx);
-			if (pld_size > 0)
-			{
-				//for (int i = 0; i < pld_size; i++)
-				//{
-				//	printf("%.2X", gl_tx_buf.u8[i]);
-				//}
-				//printf("\n");
-				float rawtemp = gl_tx_buf.i16[6];
-				float mV = ((rawtemp * 3.3f) / 4096.f)*1000.f;
-				float Temp = (5.f / 44.f) * ((5.f * sqrt(9111265.f - 1760.f * mV)) - 13501.f);
-
-				printf("Temperature C = %f\n", Temp);
-			}
-		}
-	}
-}
-
-uint32_t parsecouter = 0;
-const uint32_t num_samples_to_skip_plotting = 100;
-void parse_PPP_offaxis_encoder(uint8_t * input_buf, int payload_size, float * parsed_data, int* parsed_data_size)
-{
-	if (payload_size <= sizeof(packed_sensor_data_t))
-	{
-		packed_sensor_data_t* pData = (packed_sensor_data_t*)(&input_buf[0]);
-		float rawtemp = pData->i16[6];
-		float mV = ((rawtemp * 3.3f) / 4096.f) * 1000.f;
-		float Temp = (5.f / 44.f) * ((5.f * sqrt(9111265.f - 1760.f * mV)) - 13501.f);
-		parsed_data[0] = Temp;
-		parsed_data[1] = ((float)SDL_GetTicks64()) / 1000.f;
-		*parsed_data_size = 2;
-		//printf("Temperature C = %f, Time = %f\n", parsed_data[0], parsed_data[1]);
-	}
-}
-/*
- * Load packed 12 bit values located in an 8bit array into
- * an unpacked (zero padded) 16 bit array. FSR utility function
- */
-void unpack_8bit_into_12bit(uint8_t* arr, uint16_t* vals, int valsize)
-{
-	for (int i = 0; i < valsize; i++)
-		vals[i] = 0;	//clear the buffer before loading it with |=
-	for (int bidx = valsize * 12 - 4; bidx >= 0; bidx -= 4)
-	{
-		int validx = bidx / 12;
-		int arridx = bidx / 8;
-		int shift_val = (bidx % 8);
-		vals[validx] |= ((arr[arridx] >> shift_val) & 0x0F) << (bidx % 12);
-	}
-}
-#define NUM_FSR_PER_FINGER 6
-
-/*Packing structure for de-compressed FSRs*/
-typedef union
-{
-	uint8_t d[sizeof(uint16_t) * NUM_FSR_PER_FINGER];
-	uint16_t v[NUM_FSR_PER_FINGER];
-}fsr_fmt_t;
-
-void parse_PPP_fsr_sensor(uint8_t* input_buf, int payload_size, float* parsed_data, int* parsed_data_size)
-{
-	//for (int i = 0; i < payload_size; i++)
- //	{
-	//	printf("%X ", input_buf[i]);
-	//}
-	//printf("\r\n");
-
-	fsr_fmt_t fmt = { 0 };
-	unpack_8bit_into_12bit(input_buf, fmt.v, NUM_FSR_PER_FINGER);
-	int i = 0;
-	for (i = 0; i < NUM_FSR_PER_FINGER; i++)
-	{
-		parsed_data[i] = (float)fmt.v[i];
-		printf("%f, ", parsed_data[i]);
-	}
-	printf("\r\n");
-	parsed_data[i++] = ((float)SDL_GetTicks64()) / 1000.f;
-	*parsed_data_size = i;
-}
-
-
-void text_only(HANDLE*pSer)
-{
-	int pld_size = 0;
-	int previous_wordsize = 0;
-	int wordsize = 0;
-	int wordsize_match_count = 0;
-	while (1)
-	{
-		LPDWORD num_bytes_read = 0;
-		pld_size = 0;
-		int rc = ReadFile(*pSer, gl_ser_readbuf, 512, (LPDWORD)(&num_bytes_read), NULL);	//should be a DOUBLE BUFFER!
-		for (int i = 0; i < (int)num_bytes_read; i++)
-		{
-			uint8_t new_byte = gl_ser_readbuf[i];
-			pld_size = parse_PPP_stream(new_byte, gl_ppp_payload_buffer, PAYLOAD_SIZE, gl_ppp_unstuffing_buffer, UNSTUFFING_BUFFER_SIZE, &gl_ppp_bidx);
-			if (pld_size > 0)
-			{
-				parse_PPP_values_noscale(gl_ppp_payload_buffer, pld_size, gl_valdump, &wordsize);
-
-				//obtain consecutive matching counts
-				if (wordsize == previous_wordsize && wordsize > 0)
-				{
-					wordsize_match_count++;
-					for (int fvidx = 0; fvidx < wordsize; fvidx++)
-					{
-						if (gl_options.print_in_parser == 0)
-						{
-							float val = gl_valdump[fvidx]*gl_options.yscale;
-							if (val >= 0)
-								printf("+%0.6f", val);
-							else
-								printf("%0.6f", val);
-							
-							if (fvidx <  (wordsize - 1))
-								printf(", ");
-						}
-					}
-					printf("\n");
-				}
-				else
-				{
-					wordsize_match_count = 0;
-				}
-				previous_wordsize = wordsize;
-			}
-		}
-	}
-}
-
-
 int main(int argc, char* args[])
 {
 	parse_args(argc, args, &gl_options);
@@ -274,12 +73,7 @@ int main(int argc, char* args[])
 		if(gl_options.csv_header == 0)
 			printf("No COM ports found\n");
 	}
-	if (gl_options.print_only)
-	{
-		printf("Starting parser\n");
-		offaxis_encoder_parser(&serialport);
-		//text_only(&serialport);
-	}
+
 
 	//The window we'll be rendering to
 	SDL_Window* window = NULL;
@@ -323,7 +117,12 @@ int main(int argc, char* args[])
 			int wordsize = 0;
 			int previous_wordsize = 0;
 			int wordsize_match_count = 0;
-
+			SDL_SetRelativeMouseMode(SDL_TRUE);
+			int prev_mouse_x = SCREEN_WIDTH / 2;
+			int prev_mouse_y = SCREEN_HEIGHT / 2;
+			int32_t accum_mouse_x = 0;
+			int32_t accum_mouse_y = 0;
+			SDL_WarpMouseInWindow(window, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 			while (quit == false) 
 			{
 				uint64_t tick = SDL_GetTicks64() - start_tick;
@@ -332,89 +131,15 @@ int main(int argc, char* args[])
 				SDL_SetRenderDrawColor(pRenderer, bgColor.r, bgColor.g, bgColor.b, bgColor.a);
 				SDL_RenderClear(pRenderer);
 
+				int mouse_x, mouse_y;
+				SDL_GetMouseState(&mouse_x, &mouse_y);
+				accum_mouse_x += mouse_x - prev_mouse_x;
+				accum_mouse_y += mouse_y - prev_mouse_y;
+				printf("%d,%d\n", accum_mouse_x, accum_mouse_y);
+				SDL_WarpMouseInWindow(window, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+
 
 				uint8_t new_pkt = 0;
-				LPDWORD num_bytes_read = 0;
-				pld_size = 0;
-				int rc = ReadFile(serialport, gl_ser_readbuf, 512, (LPDWORD)(&num_bytes_read), NULL);	//should be a DOUBLE BUFFER!
-				for (int i = 0; i < (int)num_bytes_read; i++)
-				{
-					uint8_t new_byte = gl_ser_readbuf[i];
-					pld_size = parse_PPP_stream(new_byte, gl_ppp_payload_buffer, PAYLOAD_SIZE, gl_ppp_unstuffing_buffer, UNSTUFFING_BUFFER_SIZE, &gl_ppp_bidx);
-					if (pld_size > 0)
-					{
-
-						if (gl_options.offaxis_encoder)
-						{
-							parsecouter++;
-							if (parsecouter >= 100)
-							{
-								parsecouter = 0;
-
-								parse_PPP_offaxis_encoder(gl_ppp_payload_buffer, pld_size, gl_valdump, &wordsize);
-								new_pkt = 1;
-							}
-						}
-						else if (gl_options.fsr_sensor)
-						{
-							parse_PPP_fsr_sensor(gl_ppp_payload_buffer, pld_size, gl_valdump, &wordsize);
-							new_pkt = 1;
-						}
-						else
-						{
-							if (gl_options.xy_mode == 0)
-							{
-								parse_PPP_values(gl_ppp_payload_buffer, pld_size, gl_valdump, &wordsize);
-								new_pkt = 1;
-							}
-							else
-							{
-								parse_PPP_values_noscale(gl_ppp_payload_buffer, pld_size, gl_valdump, &wordsize);
-								new_pkt = 1;
-							}
-						}
-						//obtain consecutive matching counts
-						if (wordsize == previous_wordsize && wordsize > 0)
-						{
-							wordsize_match_count++;
-							if (gl_options.print_vals)
-							{
-								for (int fvidx = 0; fvidx < wordsize; fvidx++)
-								{
-									if(fvidx < wordsize-1)
-										printf("%f, ", gl_valdump[fvidx]*gl_options.yscale);
-									else
-										printf("%f\n", gl_valdump[fvidx] * gl_options.yscale);
-								}
-							}
-						}
-						else
-						{
-							wordsize_match_count = 0;
-						}
-						previous_wordsize = wordsize;
-						
-
-						//resize action
-						if(wordsize_match_count >= 100)
-						{
-							int num_lines = 0;
-							if (gl_options.xy_mode == 0)
-							{
-								num_lines = (wordsize - 1);
-							}
-							else
-							{
-								num_lines = wordsize / 2;
-							}
-							if (fpoints_lines.size() != num_lines)
-							{
-								fpoints_lines.resize(num_lines, std::vector<fpoint_t>(dbufsize));
-							}
-
-						}
-					}
-				}
 
 				{	//obtain a new xscale.
 					//float mintime = 10000000000000.f;
