582d15c9d4cc5958034d743045074a98a7e5e087
ocanath <ocanath@gmail.com>
2024-10-19 17:01:31 -0700

    eliminated rendering

--- rt-plotter-sdl/main.cpp
+++ rt-plotter-sdl/main.cpp
@@ -118,28 +118,13 @@ int main(int argc, char* args[])
 		}
 		else
 		{
-			SDL_Renderer* pRenderer = SDL_CreateRenderer(window, -1, 0);
-			SDL_SetRenderDrawColor(pRenderer, 255, 255, 255, 255);
 
 			SDL_Event e; 
 			bool quit = false; 
-			int inc = 0;
-			const int dbufsize = SCREEN_WIDTH*3;
-			std::vector<SDL_Point> points(dbufsize);
-			const int numlines = 1;
-			std::vector<std::vector<fpoint_t>> fpoints_lines(numlines, std::vector<fpoint_t>(dbufsize) );
 
-
-			float xscale = 0.f;
 
 			uint64_t start_tick = SDL_GetTicks64();
-			uint8_t serialbuffer[10] = { 0 };
-			int pld_size = 0;
 
-
-			int wordsize = 0;
-			int previous_wordsize = 0;
-			int wordsize_match_count = 0;
 			SDL_SetRelativeMouseMode(SDL_TRUE);
 			int prev_mouse_x = SCREEN_WIDTH / 2;
 			int prev_mouse_y = SCREEN_HEIGHT / 2;
@@ -151,9 +136,6 @@ int main(int argc, char* args[])
 			{
 				uint64_t tick = SDL_GetTicks64() - start_tick;
 				float t = ((float)tick) * 0.001f;
-
-				SDL_SetRenderDrawColor(pRenderer, bgColor.r, bgColor.g, bgColor.b, bgColor.a);
-				SDL_RenderClear(pRenderer);
 
 				int mouse_x, mouse_y;
 				SDL_GetMouseState(&mouse_x, &mouse_y);
@@ -190,91 +172,12 @@ int main(int argc, char* args[])
 				sendto(client.s, (const char*)gl_ppp_stuffing_buffer, stuffed_size, 0, (struct sockaddr*)&client.si_other, client.slen);
 
 
-				uint8_t new_pkt = 0;
-
-				if (gl_options.xy_mode == 0)
-				{
-					xscale = ((float)SCREEN_WIDTH) / (fpoints_lines[0][dbufsize - 1].x - fpoints_lines[0][0].x);
-				}
-
-				for (int line = 0; line < fpoints_lines.size(); line++)
-				{	
-					SDL_SetRenderDrawColor(pRenderer, template_colors[line % NUM_COLORS].r, template_colors[line % NUM_COLORS].g, template_colors[line % NUM_COLORS].b, 255);
-
-					//retrieve and load all available datapoints here
-					std::vector<fpoint_t>* pFpoints = &fpoints_lines[line];
-
-					if (new_pkt)
-					{
-						float x, y;
-						if (gl_options.xy_mode == 0)
-						{
-							/*Parsing and loading done HERE.
-							* if there is a more complex parsing function, implement it elsewhere and have it return X and Y.
-							*
-							* It should be a function whose input is the unstuffed PPP buffer and whose output is x and y of each line contained in the buffer payload
-							*/
-							x = gl_valdump[fpoints_lines.size()];
-							y = gl_valdump[line];
-						}
-						else
-						{
-							x = gl_valdump[line * 2];
-							y = gl_valdump[(line * 2) + 1];
-						}
-
-						std::rotate(pFpoints->begin(), pFpoints->begin() + 1, pFpoints->end());
-						(*pFpoints)[dbufsize - 1].x = x;
-						(*pFpoints)[dbufsize - 1].y = y;
-					}
-
-					float div_pixel_size = 0;
-					float div_center = 0;
-					if(gl_options.spread_lines)
-						div_pixel_size = (float)SCREEN_HEIGHT / ((float)fpoints_lines.size());
-					else
-					{
-						div_center = (float)SCREEN_HEIGHT / 2;
-					}
-
-					for (int i = 0; i < points.size(); i++)
-					{
-						if (gl_options.spread_lines)
-						{
-							div_center = (div_pixel_size * line) + (div_pixel_size * .5f);	//calculate the center point of the line we're drawing on screen
-						}
-						if (gl_options.xy_mode == 0)
-						{
-							points[i].x = (int)(((*pFpoints)[i].x - (*pFpoints)[0].x) * xscale);
-							points[i].y = SCREEN_HEIGHT - ((int)((*pFpoints)[i].y * gl_options.yscale) + div_center);
-						}
-						else
-						{
-							points[i].x = (int)(((*pFpoints)[i].x) * gl_options.yscale) + SCREEN_WIDTH /  2;
-							points[i].y = (-(int)((*pFpoints)[i].y * gl_options.yscale)) + SCREEN_HEIGHT / 2;	//apply uniform scaling
-						}
-					}
-
-					SDL_RenderDrawLines(pRenderer, (SDL_Point*)(&points[0]), dbufsize);
-				}
-				SDL_RenderPresent(pRenderer);
-				
 				SDL_PollEvent(&e);
 				{ 
 					if (e.type == SDL_QUIT) 
 						quit = true; 
 				} 
 			}
-
-			//erase all the buffers
-			for (int i = 0; i < fpoints_lines.size(); i++)
-			{
-				fpoints_lines[i].clear();
-			}
-			fpoints_lines.clear();
-			points.clear();
-
-			SDL_DestroyRenderer(pRenderer);
 		}
 	}
 
@@ -287,5 +190,6 @@ int main(int argc, char* args[])
 	closesocket(client.s);
 	WSACleanup();
 
+	printf("Tore everything down, exiting nicely\n");
 	return 0;
 }
