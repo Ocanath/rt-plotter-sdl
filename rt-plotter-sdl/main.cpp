29c0144d4bc47557b007642e2a47d6393ed0bcad
ocanath <ocanath@gmail.com>
2024-10-19 20:23:08 -0700

    adding timestamps, keypressses

--- rt-plotter-sdl/main.cpp
+++ rt-plotter-sdl/main.cpp
@@ -131,11 +131,33 @@ int main(int argc, char* args[])
 			int32_t accum_mouse_x = 0;
 			int32_t accum_mouse_y = 0;
 			int32_t forward = 0;
+
 			SDL_WarpMouseInWindow(window, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+
+			bool keys[322];  // 322 is the number of SDLK_DOWN events
+
+			for (int i = 0; i < 322; i++) { // init them all to false
+				keys[i] = false;
+			}
+
+			int32_t delta = 0;
+			uint64_t ms_ts = 0;
+
+			uint64_t w_ts = 0;
+			uint64_t s_ts = 0;
+			bool w_pressed_prev = false;
+			bool s_pressed_prev = false;
+
 			while (quit == false) 
 			{
 				uint64_t tick = SDL_GetTicks64() - start_tick;
+				if (tick - ms_ts > 0)
+				{
+					delta = (int32_t)(tick - ms_ts);
+					ms_ts = tick;
+				}
 				float t = ((float)tick) * 0.001f;
+
 
 				int mouse_x, mouse_y;
 				SDL_GetMouseState(&mouse_x, &mouse_y);
@@ -147,6 +169,28 @@ int main(int argc, char* args[])
 				int32_t w1 = (accum_mouse_x + forward);
 				int32_t w2 = (accum_mouse_x - forward);
 				int32_t gy = wrap_2pi_14b(accum_mouse_y);
+				
+
+				if (keys[SDLK_w])
+				{
+					forward += delta;
+				}
+				if (keys[SDLK_s])
+				{
+					forward -= delta;
+				}
+				if (keys[SDLK_w] == true && w_pressed_prev == false)
+				{
+					w_ts = tick;
+				}
+				if (keys[SDLK_s] == true && s_pressed_prev == false)
+				{
+					s_ts = tick;
+				}
+				w_pressed_prev = keys[SDLK_w];
+				s_pressed_prev = keys[SDLK_s];
+
+
 				printf("%f, %f, %f\n", (float)w1*(180.f/(float)PI_14B), (float)w2 * (180.f / (float)PI_14B), (float)gy*(180.f/(float)PI_14B));
 				SDL_WarpMouseInWindow(window, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 
@@ -176,6 +220,22 @@ int main(int argc, char* args[])
 				{ 
 					if (e.type == SDL_QUIT) 
 						quit = true; 
+					else if (e.type == SDL_KEYDOWN)
+					{
+						int idx = (int)e.key.keysym.sym;
+						if (idx >= 0 && idx <= sizeof(keys) / sizeof(bool))
+						{
+							keys[idx] = true;
+						}
+					}
+					else if (e.type == SDL_KEYUP)
+					{
+						int idx = (int)e.key.keysym.sym;
+						if (idx >= 0 && idx <= sizeof(keys) / sizeof(bool))
+						{
+							keys[idx] = false;
+						}
+					}
 				} 
 			}
 		}
