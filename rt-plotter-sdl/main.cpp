2a9931d1c0e8ade0dc758e06870c0b81c57822ff
ocanath <ocanath@gmail.com>
2024-11-15 10:11:08 -0800

    implemented hand basic communication mode in c++

--- rt-plotter-sdl/main.cpp
+++ rt-plotter-sdl/main.cpp
@@ -2,7 +2,6 @@
 and may not be redistributed without written permission.*/
 
 //Using SDL and standard IO
-#include <SDL.h>
 #include <stdio.h>
 #include <math.h>
 #include <vector>
@@ -36,16 +35,13 @@ static float gl_valdump[PAYLOAD_SIZE / sizeof(float)] = { 0 };
 Generic hex checksum calculation.
 TODO: use this in the psyonic API
 */
-uint32_t fletchers_checksum32(uint32_t* arr, int size)
+uint8_t get_checksum(uint8_t* arr, int size)
 {
-	int32_t checksum = 0;
-	int32_t fchk = 0;
+
+	int8_t checksum = 0;
 	for (int i = 0; i < size; i++)
-	{
-		checksum += (int32_t)arr[i];
-		fchk += checksum;
-	}
-	return fchk;
+		checksum += (int8_t)arr[i];
+	return -checksum;
 }
 
 
@@ -56,68 +52,48 @@ uint32_t fletchers_checksum32(uint32_t* arr, int size)
 *	parsed_data: floats, parsed from input buffer
 * Returns: number of parsed values
 */
-void parse_PPP_values_noscale(uint8_t* input_buf, int payload_size, float* parsed_data, int * parsed_data_size)
+void parse_PPP_values_noscale(uint8_t* input_buf, int payload_size, float* parsed_data, int* parsed_data_size)
 {
-	uint32_t* pbu32 = (uint32_t*)(&input_buf[0]);
-	int32_t* pbi32 = (int32_t*)(&input_buf[0]);
-	int wordsize = payload_size / sizeof(uint32_t);
-	int i = 0;
-	for (i = 0; i < wordsize; i++)
+	uint8_t vibration = input_buf[1];
+	uint8_t dc_state = input_buf[9]; 
+	uint8_t r = input_buf[4];
+	uint8_t g = input_buf[5];
+	uint8_t b = input_buf[6];
+	uint8_t chk = get_checksum(input_buf, 10);
+	if (chk == input_buf[10])
 	{
-		parsed_data[i] = ((float)pbi32[i]);
-		//printf("%d ", pbi32[i]);
+		const char* vsig_map[6] = { "DIFF_NEG", "DIFF_POS", "CO_CONTRACT", "RELAX_SIG", "PULSE_POS", "PULSE_NEG" };
+		if (dc_state >= 0 && dc_state < 6)
+		{
+			printf("Vib = %d, color = %0.2X%0.2X%0.2X, Sig = %s\n", vibration, r, g, b, vsig_map[dc_state]);
+		}
+		else
+		{
+			printf("error, dc state out of bounds\n");
+		}
 	}
-	//printf("\n");
-	//parsed_data[i] = ((float)pbu32[i]) / 1000.f;
-
-	*parsed_data_size = wordsize;
+	else
+	{
+		printf("Checksum Mismatch\n");
+	}
 }
 
 
-/*
-* Inputs:
-*	input_buf: raw unstuffed data buffer
-* Outputs:
-*	parsed_data: floats, parsed from input buffer
-* Returns: number of parsed values
-*/
-void parse_PPP_values(uint8_t* input_buf, int payload_size, float* parsed_data, int * parsed_data_size)
+uint64_t gl_txts = 0;
+void write_hand_command(HANDLE*pSer)
 {
-	uint32_t* pbu32 = (uint32_t*)(&input_buf[0]);
-	int32_t* pbi32 = (int32_t*)(&input_buf[0]);
-	int wordsize = payload_size / sizeof(uint32_t);
-	int i = 0;
-	for (i = 0; i < wordsize - 1; i++)
+	uint64_t tick = GetTickCount64();
+	if (tick - gl_txts > 10)
 	{
-		parsed_data[i] = ((float)pbi32[i]);
-		//printf("%d ", pbi32[i]);
-	}
-	//printf("\n");
-	parsed_data[i] = ((float)pbu32[i]) / 1000.f;
+		gl_txts = tick;
 
-	*parsed_data_size = wordsize;
-}
+		uint8_t buf[] = { 0x50, 0x48, 0x00 };
+		uint8_t stuff_buf[sizeof(buf) * 2 + 2] = { 0 };
 
-uint64_t dummy_loopback_txts = 0;
-void write_dummy_loopback(HANDLE*pSer)
-{
-	uint64_t tick = SDL_GetTicks64();
-	if (tick - dummy_loopback_txts > 5)
-	{
-		dummy_loopback_txts = tick;
-		double time = (double)tick / 1000.f;
-		double s1 = sin(time);
-		double c1 = cos(time);
-
-		int32_t vals[3] = { 0 };
-		uint8_t stuff_buf[sizeof(vals) * 2 + 2] = { 0 };
-		int idx = 0;
-		vals[idx++] = (int32_t)(s1 * 4096.);
-		vals[idx++] = (int32_t)(c1 * 4096.);
-		vals[idx++] = tick;
-		int num_bytes = PPP_stuff((uint8_t*)vals, sizeof(int32_t) * idx, stuff_buf, sizeof(stuff_buf));
+		buf[2] = get_checksum(buf, 2);
+		int nb = PPP_stuff(buf, sizeof(buf), stuff_buf, sizeof(stuff_buf));
 		LPDWORD written = 0;
-		int wfrc = WriteFile(*pSer, stuff_buf, num_bytes, written, NULL);
+		int wfrc = WriteFile(*pSer, stuff_buf, nb, written, NULL);
 	}
 }
 
@@ -132,7 +108,7 @@ void text_only(HANDLE*pSer)
 	{
 		if (gl_options.write_dummy_loopback)
 		{
-			write_dummy_loopback(pSer);
+			write_hand_command(pSer);
 		}
 		LPDWORD num_bytes_read = 0;
 		pld_size = 0;
@@ -144,32 +120,6 @@ void text_only(HANDLE*pSer)
 			if (pld_size > 0)
 			{
 				parse_PPP_values_noscale(gl_ppp_payload_buffer, pld_size, gl_valdump, &wordsize);
-
-				//obtain consecutive matching counts
-				if (wordsize == previous_wordsize && wordsize > 0)
-				{
-					wordsize_match_count++;
-					for (int fvidx = 0; fvidx < wordsize; fvidx++)
-					{
-						if (gl_options.print_in_parser == 0)
-						{
-							float val = gl_valdump[fvidx]*gl_options.parser_yscale;
-							if (val >= 0)
-								printf("+%0.6f", val);
-							else
-								printf("%0.6f", val);
-							
-							if (fvidx <  (wordsize - 1))
-								printf(", ");
-						}
-					}
-					printf("\n");
-				}
-				else
-				{
-					wordsize_match_count = 0;
-				}
-				previous_wordsize = wordsize;
 			}
 		}
 	}
@@ -202,234 +152,8 @@ int main(int argc, char* args[])
 		if(gl_options.csv_header == 0)
 			printf("No COM ports found\n");
 	}
-	if (gl_options.print_only)
-	{
-		text_only(&serialport);
-	}
 
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
-
-	SDL_Color bgColor = { 10, 10, 10, 255 };
-
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
-		{
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-		}
-		else
-		{
-			SDL_Renderer* pRenderer = SDL_CreateRenderer(window, -1, 0);
-			SDL_SetRenderDrawColor(pRenderer, 255, 255, 255, 255);
-
-			SDL_Event e; 
-			bool quit = false; 
-			int inc = 0;
-			const int dbufsize = SCREEN_WIDTH*gl_options.num_widths;
-			std::vector<SDL_Point> points(dbufsize);
-			const int numlines = 1;
-			std::vector<std::vector<fpoint_t>> fpoints_lines(numlines, std::vector<fpoint_t>(dbufsize) );
-
-
-			float xscale = 0.f;
-
-			uint64_t start_tick = SDL_GetTicks64();
-			uint8_t serialbuffer[10] = { 0 };
-			int pld_size = 0;
-
-
-			int wordsize = 0;
-			int previous_wordsize = 0;
-			int wordsize_match_count = 0;
-			int cycle_count_for_printing = 0;
-			while (quit == false) 
-			{
-				uint64_t tick = SDL_GetTicks64() - start_tick;
-				float t = ((float)tick) * 0.001f;
-
-				SDL_SetRenderDrawColor(pRenderer, bgColor.r, bgColor.g, bgColor.b, bgColor.a);
-				SDL_RenderClear(pRenderer);
-
-				if (gl_options.write_dummy_loopback)
-				{
-					write_dummy_loopback(&serialport);
-				}
-
-				LPDWORD num_bytes_read = 0;
-				pld_size = 0;
-				int rc = ReadFile(serialport, gl_ser_readbuf, 512, (LPDWORD)(&num_bytes_read), NULL);	//should be a DOUBLE BUFFER!
-				for (int i = 0; i < (int)num_bytes_read; i++)
-				{
-					uint8_t new_byte = gl_ser_readbuf[i];
-					pld_size = parse_PPP_stream(new_byte, gl_ppp_payload_buffer, PAYLOAD_SIZE, gl_ppp_unstuffing_buffer, UNSTUFFING_BUFFER_SIZE, &gl_ppp_bidx);
-					if (pld_size > 0)
-					{
-						if (gl_options.xy_mode == 0)
-						{
-							parse_PPP_values(gl_ppp_payload_buffer, pld_size, gl_valdump, &wordsize);
-						}
-						else
-						{
-							parse_PPP_values_noscale(gl_ppp_payload_buffer, pld_size, gl_valdump, &wordsize);
-						}
-						
-						//obtain consecutive matching counts
-						if (wordsize == previous_wordsize && wordsize > 0)
-						{
-							wordsize_match_count++;
-							if (gl_options.print_vals)
-							{
-								cycle_count_for_printing = (cycle_count_for_printing + 1) % gl_options.print_in_parser_every_n;
-								if (cycle_count_for_printing == 0)
-								{
-									for (int fvidx = 0; fvidx < wordsize; fvidx++)
-									{
-										printf("%f, ", gl_valdump[fvidx] * gl_options.parser_yscale);
-									}
-									printf("\n");
-								}
-							}
-						}
-						else
-						{
-							wordsize_match_count = 0;
-						}
-						previous_wordsize = wordsize;
-						
-
-						//resize action
-						if(wordsize_match_count >= 100)
-						{
-							int num_lines = 0;
-							if (gl_options.xy_mode == 0)
-							{
-								num_lines = (wordsize - 1);
-							}
-							else
-							{
-								num_lines = wordsize / 2;
-							}
-							if (fpoints_lines.size() != num_lines)
-							{
-								fpoints_lines.resize(num_lines, std::vector<fpoint_t>(dbufsize));
-							}
-
-						}
-					}
-				}
-
-				{	//obtain a new xscale.
-					//float mintime = 10000000000000.f;
-					//float maxtime = 0.f;
-					//for (int line = 0; line < fpoints_lines.size(); line++)
-					//{
-					//	float max_candidate = fpoints_lines[line][dbufsize - 1].x;
-					//	float min_candidate = fpoints_lines[line][0].x;
-					//	if (max_candidate > maxtime)
-					//		maxtime = max_candidate;
-					//	if (min_candidate < mintime)
-					//		mintime = min_candidate;
-					//}
-					if (gl_options.xy_mode == 0)
-					{
-						xscale = ((float)SCREEN_WIDTH) / (fpoints_lines[0][dbufsize - 1].x - fpoints_lines[0][0].x);
-					}
-				}
-
-
-				for (int line = 0; line < fpoints_lines.size(); line++)
-				{	
-					SDL_SetRenderDrawColor(pRenderer, template_colors[line % NUM_COLORS].r, template_colors[line % NUM_COLORS].g, template_colors[line % NUM_COLORS].b, 255);
-
-					//retrieve and load all available datapoints here
-					std::vector<fpoint_t>* pFpoints = &fpoints_lines[line];
-
-
-					float x, y;
-					if (gl_options.xy_mode == 0)
-					{
-						/*Parsing and loading done HERE.
-						* if there is a more complex parsing function, implement it elsewhere and have it return X and Y.
-						*
-						* It should be a function whose input is the unstuffed PPP buffer and whose output is x and y of each line contained in the buffer payload
-						*/
-						x = gl_valdump[fpoints_lines.size()];
-						y = gl_valdump[line];
-					}
-					else
-					{
-						x = gl_valdump[line * 2];
-						y = gl_valdump[(line * 2) + 1];
-					}
-					
-					std::rotate(pFpoints->begin(), pFpoints->begin() + 1, pFpoints->end());
-					(*pFpoints)[dbufsize - 1].x = x;
-					(*pFpoints)[dbufsize - 1].y = y;
-
-
-					float div_pixel_size = 0;
-					float div_center = 0;
-					if(gl_options.spread_lines)
-						div_pixel_size = (float)SCREEN_HEIGHT / ((float)fpoints_lines.size());
-					else
-					{
-						div_center = (float)SCREEN_HEIGHT / 2;
-					}
-
-					for (int i = 0; i < points.size(); i++)
-					{
-						if (gl_options.spread_lines)
-						{
-							div_center = (div_pixel_size * line) + (div_pixel_size * .5f);	//calculate the center point of the line we're drawing on screen
-						}
-						if (gl_options.xy_mode == 0)
-						{
-							points[i].x = (int)(((*pFpoints)[i].x - (*pFpoints)[0].x) * xscale);
-							points[i].y = SCREEN_HEIGHT - ((int)((*pFpoints)[i].y * gl_options.yscale) + div_center);
-						}
-						else
-						{
-							points[i].x = (int)(((*pFpoints)[i].x) * gl_options.yscale) + SCREEN_WIDTH /  2;
-							points[i].y = (-(int)((*pFpoints)[i].y * gl_options.yscale)) + SCREEN_HEIGHT / 2;	//apply uniform scaling
-						}
-					}
-
-					SDL_RenderDrawLines(pRenderer, (SDL_Point*)(&points[0]), dbufsize);
-				}
-				SDL_RenderPresent(pRenderer);
-				
-				SDL_PollEvent(&e);
-				{ 
-					if (e.type == SDL_QUIT) 
-						quit = true; 
-				} 
-			}
-
-			//erase all the buffers
-			for (int i = 0; i < fpoints_lines.size(); i++)
-			{
-				fpoints_lines[i].clear();
-			}
-			fpoints_lines.clear();
-			points.clear();
-
-			SDL_DestroyRenderer(pRenderer);
-		}
-	}
-
-	//Destroy window
-	SDL_DestroyWindow(window);
-
-	//Quit SDL subsystems
-	SDL_Quit();
+	text_only(&serialport);
 
 	//close serial port
 	CloseHandle(serialport);
