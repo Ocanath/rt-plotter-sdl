f16922b13577d9f429f890a118cab23acb0a2e5a
ocanath <ocanath@gmail.com>
2025-04-25 08:43:24 -0700

    compiles now

--- ppp-parsing.h
+++ ppp-parsing.h
@@ -5,11 +5,6 @@
 #define PAYLOAD_SIZE 512
 #define UNSTUFFING_BUFFER_SIZE (PAYLOAD_SIZE * 2 + 2)
 
-extern int gl_ppp_bidx;
-extern uint8_t gl_ppp_payload_buffer[PAYLOAD_SIZE];	//buffer
-extern uint8_t gl_ppp_unstuffing_buffer[UNSTUFFING_BUFFER_SIZE];
-extern uint8_t gl_ser_readbuf[PAYLOAD_SIZE];
-extern float gl_valdump[PAYLOAD_SIZE / sizeof(float)];
 
 
 void parse_PPP_values_noscale(uint8_t* input_buf, int payload_size, float* parsed_data, int* parsed_data_size);
