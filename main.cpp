dec5ae033606d2918b98502aa8b72513eb1fda84
ocanath <ocanath@gmail.com>
2025-05-26 08:26:21 -0700

    checked out catcar controller

--- main.cpp
+++ main.cpp
@@ -1,27 +1,26 @@
+/*This source code copyrighted by Lazy Foo' Productions 2004-2023
+and may not be redistributed without written permission.*/
+
+//Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
-#include <vector>
-
-// #include<winsock2.h>
-// #include <WS2tcpip.h>
-// #include "WinUdpClient.h"
+#include<winsock2.h>
+#include <WS2tcpip.h>
+#include "WinUdpClient.h"
 
 #include <math.h>
-
-#ifdef PLATFORM_WINDOWS
+#include <vector>
 #include "winserial.h"
-#elif defined(PLATFORM_LINUX)
-#include "linux-serial.h"
-#endif
-
 #include "PPP.h"
 #include "colors.h"
 #include "args-parsing.h"
 #include <algorithm>
 #include "trig_fixed.h"
-#include "sauron-eye-closedform-ik.h"
-#include "ppp-parsing.h"
-// #pragma comment(lib,"ws2_32.lib") //Winsock Library
+
+
+#pragma comment(lib,"ws2_32.lib") //Winsock Library
+
+
 enum { POSITION = 0xFA, TURBO = 0xFB, STEALTH = 0xFC };
 
 #define PAYLOAD_SIZE 512
@@ -45,6 +44,37 @@ static uint8_t gl_ser_readbuf[512] = { 0 };
 static float gl_valdump[PAYLOAD_SIZE / sizeof(float)] = { 0 };
 
 
+/*
+Generic hex checksum calculation.
+TODO: use this in the psyonic API
+*/
+uint32_t fletchers_checksum32(uint32_t* arr, int size)
+{
+	int32_t checksum = 0;
+	int32_t fchk = 0;
+	for (int i = 0; i < size; i++)
+	{
+		checksum += (int32_t)arr[i];
+		fchk += checksum;
+	}
+	return fchk;
+}
+
+/*
+Generic hex checksum calculation.
+TODO: use this in the psyonic API
+*/
+uint16_t fletchers_checksum16(uint16_t* arr, int size)
+{
+	int16_t checksum = 0;
+	int16_t fchk = 0;
+	for (int i = 0; i < size; i++)
+	{
+		checksum += (int16_t)arr[i];
+		fchk += checksum;
+	}
+	return fchk;
+}
 
 
 /*Value clamping symmetric about zero*/
@@ -60,22 +90,19 @@ int32_t symm_thresh(int32_t sig, int32_t thresh)
 int main(int argc, char* args[])
 {
 	parse_args(argc, args, &gl_options);
-	autoconnect_serial();//grab a serial port
 
-	// WinUdpClient client(6702);
-	// struct sockaddr_in server;
-	// server.sin_family = AF_INET;
-	// server.sin_addr = in4addr_any;
-	// server.sin_port = htons(6702);
-	// client.set_nonblocking();
-	// client.si_other.sin_family = AF_INET;
-	// //client.si_other.sin_addr.S_un.S_addr = inet_addr("192.168.123.255");
-	// //TODO: auto address discovery with WHO_GOES_THERE
-	// inet_pton(AF_INET, "192.168.137.145", &client.si_other.sin_addr);
-	// sendto(client.s, (const char*)"SPAM_ME", 7, 0, (struct sockaddr*)&client.si_other, client.slen);
-	// bind(client.s, (struct sockaddr*)&server, sizeof(server));
-
-
+	WinUdpClient client(6701);
+	struct sockaddr_in server;
+	server.sin_family = AF_INET;
+	server.sin_addr = in4addr_any;
+	server.sin_port = htons(6701);
+	client.set_nonblocking();
+	client.si_other.sin_family = AF_INET;
+	//client.si_other.sin_addr.S_un.S_addr = inet_addr("192.168.123.255");
+	//TODO: auto address discovery with WHO_GOES_THERE
+	inet_pton(AF_INET, "192.168.123.86", &client.si_other.sin_addr);
+	sendto(client.s, (const char*)"SPAM_ME", 7, 0, (struct sockaddr*)&client.si_other, client.slen);
+	bind(client.s, (struct sockaddr*)&server, sizeof(server));
 
 	//The window we'll be rendering to
 	SDL_Window* window = NULL;
@@ -110,18 +137,12 @@ int main(int argc, char* args[])
 			int32_t accum_mouse_x = 0;
 			int32_t accum_mouse_y = 0;
 			double forward = 0;
-			const double slow_speedcap = 200;
-			const double fast_speedcap = 3000;
-			double velocity_threshold = slow_speedcap;
-			double th1 = 0;
-			double th2 = 0;
 
 			SDL_WarpMouseInWindow(window, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 
-			const int num_keys = 322;
-			bool keys[num_keys];  // 322 is the number of SDLK_DOWN events
+			bool keys[322];  // 322 is the number of SDLK_DOWN events
 
-			for (int i = 0; i < num_keys; i++) { // init them all to false
+			for (int i = 0; i < 322; i++) { // init them all to false
 				keys[i] = false;
 			}
 
@@ -130,20 +151,9 @@ int main(int argc, char* args[])
 
 			uint64_t w_ts = 0;
 			uint64_t s_ts = 0;
-			uint64_t w_stop_ts = 0;
-			uint64_t s_stop_ts = 0;
 			bool w_pressed_prev = false;
 			bool s_pressed_prev = false;
-			int zerocount = 0;
-			uint64_t mouse_activity_ts = 0;
-			uint64_t print_ts = 0;
-			uint64_t udp_tx_ts = 0;
-			int32_t deltax_lastvalid = 0;
-			uint8_t throttle = 0;
-			uint8_t prev_throttle = 0;
-			uint64_t throttle_stop_ts = 0;
-			double targx = 0;
-			double targy = 0;
+
 			while (quit == false) 
 			{
 				uint64_t tick = SDL_GetTicks64() - start_tick;
@@ -154,126 +164,91 @@ int main(int argc, char* args[])
 				}
 				float t = ((float)tick) * 0.001f;
 
-				uint8_t mouse_motion = 0;
-				SDL_PollEvent(&e);
-				{
-					if (e.type == SDL_QUIT)
-						quit = true;
-					else if (e.type == SDL_MOUSEMOTION)
-					{
-						mouse_motion = 1;
-						mouse_activity_ts = tick;
-					}
-				}
 
 				int mouse_x, mouse_y;
-				uint32_t bts = SDL_GetMouseState(&mouse_x, &mouse_y);
-				int32_t gain_x = 1;
-				int32_t gain_y = -1;
-				int32_t deltax = (mouse_x - prev_mouse_x)* gain_x;
-				int32_t deltay = (mouse_y - prev_mouse_y) * gain_y;
-
-				accum_mouse_x = ( (accum_mouse_x + deltax) );
-				accum_mouse_y = ( ( accum_mouse_y + deltay) );
+				SDL_GetMouseState(&mouse_x, &mouse_y);
+				int32_t gain_x = 20;
+				int32_t gain_y = 10;
+				accum_mouse_x = ( (accum_mouse_x + (mouse_x - prev_mouse_x)* gain_x) );
+				accum_mouse_y = ( ( accum_mouse_y + (mouse_y - prev_mouse_y)* gain_y) );
+				accum_mouse_y = symm_thresh(accum_mouse_y, PI_14B / 2);
+				int32_t w1 = (-accum_mouse_x + (int32_t)forward);
+				int32_t w2 = (-accum_mouse_x - (int32_t)forward);
+				int32_t gy = wrap_2pi_14b(accum_mouse_y);
 				
-				if (mouse_motion != 0 || (tick - mouse_activity_ts) > 10)
+				double accel_thresh = 10.0;
+				if (keys[SDLK_w])
 				{
-					deltax_lastvalid = deltax;
+					double dt = (double)(tick - w_ts) * 0.001;
+					if (dt > accel_thresh)
+						dt = accel_thresh;
+					forward += (double)delta * dt;
 				}
-
-
-
-				double kbvx = -(double)accum_mouse_x / 1000.;
-				double kbvy = (double)accum_mouse_y / 1000.;
-
-				double vx = kbvx;
-				double vy = kbvy;
-				if (bts == 1)	//left mouse button
+				if (keys[SDLK_s])
 				{
-					vx = targx;
-					vy = targy;
+					double dt = (double)(tick - s_ts) * 0.001;
+					if (dt > accel_thresh)
+						dt = accel_thresh;
+					forward -= (double)delta * dt;
 				}
-				if (bts == 2)	//center mouse button
+				if (keys[SDLK_w] == true && w_pressed_prev == false)
 				{
-					targx = vx;
-					targy = vy;
+					w_ts = tick;
 				}
-
-				//double tsec = double(tick) / 1000;
-				//double vx = sin(tsec)*0.5;
-				//double vy = cos(tsec)*0.5;
-
-				//double vx = 0;
-				//double vy = 0;
-				//uint32_t tmod = tick % 4000;
-				//
-				//if (tmod >= 0 && tmod < 1000)
-				//{
-				//	double tmodsec = (double)(tmod - 0) / 1000.;
-				//	vx = tmodsec*10;
-				//	vy = 0;
-				//}
-				//else if (tmod >= 1000 && tmod < 2000)
-				//{
-				//	double tmodsec = (double)(tmod - 1000) / 1000.;
-				//	vx = 10;
-				//	vy = tmodsec * 10;
-				//}
-				//else if (tmod >= 2000 && tmod < 3000)
-				//{
-				//	double tmodsec = (double)(tmod - 2000) / 1000.;
-				//	vx = 10 - tmodsec * 10;
-				//	vy = 10;
-				//}
-				//else if (tmod >= 3000 && tmod < 4000)
-				//{
-				//	double tmodsec = (double)(tmod - 3000) / 1000.;
-				//	vy = 10 - tmodsec * 10;
-				//	vx = 0;
-				//}
-				//vx = vx + kbvx;
-				//vy = vy + kbvy;
-
-				const double rotangle = M_PI / 4;
-				double vxr = vx * cos(rotangle) - vy * sin(rotangle);
-				double vyr = vx * sin(rotangle) + vy * cos(rotangle);
-				get_ik_angles_double(vxr, vyr, 10, &th1, &th2);
-				int32_t th1_i32 = (int32_t)(th1 * (float)PI_14B);
-				int32_t th2_i32 = (int32_t)(th2 * (float)PI_14B);
-
-				if ( (tick - print_ts) > 50)
+				if (keys[SDLK_s] == true && s_pressed_prev == false)
 				{
-					printf("%f, %f, %d, %d, %d\n", vx, vy, th1_i32, th2_i32, bts);
-					print_ts = tick;
+					s_ts = tick;
 				}
+				w_pressed_prev = keys[SDLK_w];
+				s_pressed_prev = keys[SDLK_s];
 
+
+				printf("%f, %f, %f\n", (float)w1*(180.f/(float)PI_14B), (float)w2 * (180.f / (float)PI_14B), (float)gy*(180.f/(float)PI_14B));
 				SDL_WarpMouseInWindow(window, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 
+				uint8_t pld[32] = { 0 };
+				uint16_t* pu16 = (uint16_t*)(&pld[0]);
+				int i = 0;
+				pld[i++] = POSITION;
+				pld[i++] = 0;
+				
+				int32_t* p_cmd32 = (int32_t *)(& pld[i]);
+				p_cmd32[0] = w1;
+				i += sizeof(int32_t);
+				p_cmd32[1] = w2;
+				i += sizeof(int32_t);
+				p_cmd32[2] = gy;
+				i += sizeof(int32_t);
 
-				if (tick - udp_tx_ts > 10)
-				{
-					udp_tx_ts = tick;
+				int chkidx = (i + (i % 2)) / 2;	//pad a +1 byte if it's odd, divide by 2, set that as the start of our 16bit checksum
+				pu16[chkidx] = fletchers_checksum16(pu16, chkidx);
+				chkidx++;
+				int pld_size = chkidx * sizeof(uint16_t);
+				int stuffed_size = PPP_stuff(pld, pld_size, gl_ppp_stuffing_buffer, sizeof(gl_ppp_stuffing_buffer));
+				sendto(client.s, (const char*)gl_ppp_stuffing_buffer, stuffed_size, 0, (struct sockaddr*)&client.si_other, client.slen);
 
-					uint8_t pld[32] = { 0 };
-					uint16_t* pu16 = (uint16_t*)(&pld[0]);
-					int i = 0;
-					pld[i++] = POSITION;
-					pld[i++] = 0;
 
-					int32_t* p_cmd32 = (int32_t*)(&pld[i]);
-					p_cmd32[0] = th1_i32;
-					i += sizeof(int32_t);
-					p_cmd32[1] = th2_i32;
-					i += sizeof(int32_t);
-
-					int chkidx = (i + (i % 2)) / 2;	//pad a +1 byte if it's odd, divide by 2, set that as the start of our 16bit checksum
-					pu16[chkidx] = fletchers_checksum16(pu16, chkidx);
-					chkidx++;
-					int pld_size = chkidx * sizeof(uint16_t);
-					int stuffed_size = PPP_stuff(pld, pld_size, gl_ppp_stuffing_buffer, sizeof(gl_ppp_stuffing_buffer));
-					//sendto(client.s, (const char*)gl_ppp_stuffing_buffer, stuffed_size, 0, (struct sockaddr*)&client.si_other, client.slen);
-					serial_write(gl_ppp_stuffing_buffer, stuffed_size);
-				}
+				SDL_PollEvent(&e);
+				{ 
+					if (e.type == SDL_QUIT) 
+						quit = true; 
+					else if (e.type == SDL_KEYDOWN)
+					{
+						int idx = (int)e.key.keysym.sym;
+						if (idx >= 0 && idx <= sizeof(keys) / sizeof(bool))
+						{
+							keys[idx] = true;
+						}
+					}
+					else if (e.type == SDL_KEYUP)
+					{
+						int idx = (int)e.key.keysym.sym;
+						if (idx >= 0 && idx <= sizeof(keys) / sizeof(bool))
+						{
+							keys[idx] = false;
+						}
+					}
+				} 
 			}
 		}
 	}
@@ -284,9 +259,9 @@ int main(int argc, char* args[])
 	//Quit SDL subsystems
 	SDL_Quit();
 
-	// closesocket(client.s);
-	// WSACleanup();
-	close_serial();
+	closesocket(client.s);
+	WSACleanup();
+
 	printf("Tore everything down, exiting nicely\n");
 	return 0;
 }
